8f0b553ff65d83d0f1bc8426819f1559213df609
Brandon Huerta <bran.7594@gmail.com>
2024-02-23 23:36:50 -0600

    configuracion de clases

---
+++ Book.cs
@@ -0,0 +1,12 @@
+public class Book
+{
+    public string Title {get; set;}
+    public int PageCount { get; set; }
+    public string Status { get; set; }
+    public DateTime PublishedDate { get; set; }
+    public string thumbnailUrl { get; set; }
+    public string shortDescripcion { get; set; }
+    public string[] Autors { get; set; }
+    public string[] Categories { get; set; }
+
+}
