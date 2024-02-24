d27b808a84ac24a7cf6da53ceb924c7c4744bc3a
Brandon Huerta <bran.7594@gmail.com>
2024-02-23 23:23:49 -0600

    primer commit con prueba link para ver como filtrar palabras e imprimirlas

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+var frutas = new String[]{ "sandia", "fresa", "Mango", "Mango de azucar", "Mango Tomy"};
+var MangoList = frutas.Where(p=> p.StartsWith("Mango")).ToList();
+MangoList.ForEach(p => Console.WriteLine(p));
+
